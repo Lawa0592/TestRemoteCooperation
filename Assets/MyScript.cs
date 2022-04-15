f2e2651caa7994890ae2bb8e518cac0c287a7b03
Liweicong <congcongcba@163.com>
2022-04-15 11:48:37 +0800

    首次添加脚本MyScript

---
+++ Assets/MyScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyFunction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MyFunction()
+    {
+        // ************************************
+        // *THis is the first time edit script*
+        // ************************************
+    }
+}
